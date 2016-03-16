a7f4e8e591c4f3f5b528b46927491ec8f11d8060
KennardVG <kennard.vangrinsven@student.fontys.nl>
2016-03-17 00:29:23 +0100

    Final version
    Everything works! :)

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SocksTcpClient
 {
@@ -10,9 +10,33 @@ namespace SocksTcpClient
     {
         static void Main(string[] args)
         {
-            SocksTcpClient client = new SocksTcpClient();
-            client.StatusChanged += SocksTcpClient_OnStatusChanged;
-            client.Connect("206.192.215.78", 10000, "88.159.9.139", 1234);
+            SocksTcpClient.StatusChanged += SocksTcpClient_OnStatusChanged;
+            TcpClient client = SocksTcpClient.Connect("91.121.105.48", 1080, "94.215.86.127", 65432);
+
+            NetworkStream stream = client.GetStream();
+
+            Thread trd = new Thread(() =>
+            {
+                while (true)
+                {
+                    if (stream.DataAvailable && stream.CanRead)
+                    {
+                        byte[] responseBytes = new byte[8192];
+                        int responseLength = stream.Read(responseBytes, 0, responseBytes.Length);
+                        string response = Encoding.ASCII.GetString(responseBytes, 0, responseLength - 1);
+                        Console.WriteLine(response);
+                    }
+                }
+            });
+            trd.Start();
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("GET http://www.seafight.nl/ HTTP/1.1");
+            builder.AppendLine("");
+
+            byte[] requestBytes = Encoding.ASCII.GetBytes(builder.ToString());
+            stream.Write(requestBytes, 0, requestBytes.Length);
+            stream.Flush();
 
             Console.ReadLine();
         }
--- SocksTcpClient.cs
+++ SocksTcpClient.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Net.Sockets;
 using System.Net;
-using System.IO;
 
 namespace SocksTcpClient
 {
     public delegate void ClientStatusChangedEvent(object sender, string status);
-
-    class SocksTcpClient : TcpClient
+    
+    class SocksTcpClient
     {
-        public event ClientStatusChangedEvent StatusChanged;
+        public static event ClientStatusChangedEvent StatusChanged;
+
         private enum SocksType
         {
+            Socks4 = 0x04,
             Socks5 = 0x05
         }
 
@@ -27,7 +24,7 @@ namespace SocksTcpClient
             NoAcceptableMethods = 0xFF
         }
 
-        private string[] errorMsgs = new string[]
+        private static string[] errorMsgs = new string[]
         {
             "Successfully connected to remote host!",
             "General SOCKS server failure!",
@@ -39,87 +36,80 @@ namespace SocksTcpClient
             "Command not supported!",
             "Address type not supported!"
         };
-
-        public SocksTcpClient() { }
-
-        public void Connect(string proxyHost, int proxyPort, string targetHost, int targetPort)
+        
+        public static TcpClient Connect(string proxyHost, int proxyPort, string targetHost, int targetPort)
         {
+            TcpClient SocksClient = new TcpClient();
+
             IPAddress proxyHostAddress;
             if (!IPAddress.TryParse(proxyHost, out proxyHostAddress))
                 proxyHostAddress = Dns.GetHostAddresses(proxyHost)[0];
             IPEndPoint proxyEndPoint = new IPEndPoint(proxyHostAddress, proxyPort);
 
-            Socket socket = base.Client;
+            IPAddress targetHostAddress;
+            if (!IPAddress.TryParse(targetHost, out targetHostAddress))
+                targetHostAddress = Dns.GetHostAddresses(targetHost).Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault();
+            
+            Socket socket = SocksClient.Client;
             socket.Connect(proxyEndPoint);
 
             socket = Negotiate(socket);
             if (socket == null)
-                return;
+                return null;
 
-            socket = Handshake(socket, targetHost, targetPort);
+            socket = Handshake(socket, targetHostAddress, targetPort);
+
+            return SocksClient;
         }
-
-        private Socket Negotiate(Socket socket)
+        
+        private static Socket Negotiate(Socket socket)
         {
             byte[] requestBytes = new byte[] { (byte)SocksType.Socks5, 0x03, (byte)Authentication.None, (byte)Authentication.GSSApi, (byte)Authentication.NoAcceptableMethods };
             byte[] responseBytes = new byte[2];
 
             socket.Send(requestBytes, requestBytes.Length, SocketFlags.None);
             int bytesReceived = socket.Receive(responseBytes, 2, SocketFlags.None);
-
-            if (bytesReceived != 2)
-            {
-                OnStatusChanged(socket, "Failed to negotiate with the proxy!");
-                return null;
-            }
-
-            if (responseBytes[1] == (byte)Authentication.None)
+            
+            if (bytesReceived == 2 && responseBytes[1] == (byte)Authentication.None)
             {
                 OnStatusChanged(socket, "Successfully negotiated with the proxy!");
                 return socket;
             }
 
+            OnStatusChanged(socket, "Failed to negotiate with the proxy!");
             return null;
         }
 
-        private Socket Handshake(Socket socket, string targetHost, int targetPort)
+        private static Socket Handshake(Socket socket, IPAddress targetHost, int targetPort)
         {
             int i = 0;
-            byte[] addressBytes = Encoding.Default.GetBytes(targetHost);
-            byte[] portBytes = BitConverter.GetBytes(targetPort);
-            byte[] requestBytes = new byte[addressBytes.Length + portBytes.Length + 4];
+            byte[] addressBytes = targetHost.GetAddressBytes();
+            byte[] portBytes = new byte[2] { (byte)(targetPort / 256), (byte)(targetPort % 256) };
+            byte[] requestBytes = new byte[4 + addressBytes.Length + portBytes.Length];
             byte[] responseBytes = new byte[2];
             requestBytes[i++] = (byte)SocksType.Socks5;
             requestBytes[i++] = 0x01;
             requestBytes[i++] = 0x00;
             requestBytes[i++] = 0x01;
-
+            
             addressBytes.CopyTo(requestBytes, i);
             i += addressBytes.Length;
 
-            for (int b = portBytes.Length - 1; b >= 0; b--)
+            for (int b = 0; b < portBytes.Length; b++)
                 requestBytes[i++] = portBytes[b];
 
             socket.Send(requestBytes, i, SocketFlags.None);
-            int receivedBytes = socket.Receive(responseBytes, 2, SocketFlags.None);
-
-            if (receivedBytes != 2)
-            {
-                OnStatusChanged(socket, "Bad proxy!");
-                return null;
-            }
+            int receivedBytes = socket.Receive(responseBytes, SocketFlags.None);
 
             OnStatusChanged(socket, errorMsgs[responseBytes[1]]);
 
-            if (responseBytes[1] == 0x00)
-            {
-                return socket;
-            }
-
-            return null;
+            if (responseBytes[1] != 0x00)
+                return null;
+            
+            return socket;
         }
 
-        private void OnStatusChanged(object sender, string status)
+        private static void OnStatusChanged(object sender, string status)
         {
             ClientStatusChangedEvent handler = StatusChanged;
             if (handler != null)
